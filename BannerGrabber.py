ed7540bd185c4473edde108630b3b7d52ba76cc6
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2022-08-26 12:18:05 +0300

    Update BannerGrabber.py

--- BannerGrabber.py
+++ BannerGrabber.py
@@ -1,10 +1,4 @@
 
-import socket
-
-s = socket.socket()
-
-ip = input("Please enter the IP: ")
-port = str(input("Please enter the port: "))
-
-s.connect((ip, int(port)))
-print(s.recv(1024))
+def main():
+    ip = input("Please enter the IP: ")
+    port = str(input("Please enter the port: "))
